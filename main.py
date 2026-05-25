62e6be99f13f6241a4f53ec0b7c197f9e8d37e8a
Max <msvet813@gmail.com>
2026-05-25 13:17:19 +0300

    version 2.0

--- main.py
+++ main.py
@@ -1,17 +1,6 @@
 import os
 import json
 
-recipes = []
-
-if os.path.exists('recipes.json'):
-    with open('recipes.json', 'r', encoding='utf-8') as f:
-        try:
-            recipes = json.load(f)
-        except json.JSONDecodeError:
-            # Если файл оказался пустым или «битым»,
-            #  мы просто говорим: «Окей, пусть список будет пустым»
-            recipes = []
-
 class RecipeNotFoundError(Exception):
     pass
 class InvalidRecipeError(Exception):
@@ -19,57 +8,94 @@ class InvalidRecipeError(Exception):
 class InvalidCookingTimeError(Exception):
     pass
 
-def create_recipe(recipe: dict):
+
+class Recipe:
+    def __init__(self, title: str, instructions: str, time: int, ingredients: dict | None = None, id: int | None = None):
+        self.title = title
+        self.time = time
+        self.instructions = instructions
+        self.id = id
+        if ingredients is None:
+            self.ingredients = {}
+        else:
+            self.ingredients = ingredients
+    
+    def __repr__(self):
+        return f"Recipe(id={self.id}, title='{self.title}', time={self.time})"
+
+    def __str__(self):
+        return (
+        f"ID рецепта: {self.id}\n"
+        f"Название рецепта: {self.title}\n"
+        f"Время приготовления: {self.time} мин.\n"
+        f"Необходимые ингредиенты: {self.ingredients}\n"
+        f"Инструкция приготовления: {self.instructions}"
+    )
+
+    def __eq__(self, other):
+        if not isinstance(other, Recipe):
+            return False
+        
+        return self.title == other.title and self.time == other.time
+    
+    def __hash__(self):
+        return hash((self.title, self.time))
+
+
+def load_recipes():
+    recipes = []
+    if os.path.exists('recipes.json'):
+        try:
+            with open('recipes.json', 'r', encoding='utf-8') as raw_data:
+                load_json_recipes = json.load(raw_data)
+                for recipe in load_json_recipes:
+                    recipe1 = Recipe(**recipe)
+                    recipes.append(recipe1)
+        except json.JSONDecodeError:
+            print('Битый json')
+    return recipes
+recipes = load_recipes()
+
+
+def create_recipe(recipe: Recipe):
 
     # Валидация
-    if 'title' not in recipe:
+    if not recipe.title.strip(): # if not True = False / if not False = True
         raise InvalidRecipeError
-    if recipe['time'] <= 0:
+    if recipe.time <= 0:
         raise InvalidCookingTimeError
-
-    # Проверяем пустой ли recipes
+    
+    # Определение ID рецепта
     if recipes:
-        new_id = recipes[-1]['id'] + 1
+        recipe.id = recipes[-1].id + 1
     else:
-        new_id = 1
+        recipe.id = 1
 
-    new_recipe = {"id": new_id, **recipe}
-    recipes.append(new_recipe)
+    # Добавление в локальный список рецептов
+    recipes.append(recipe)
 
+    # Превращаем список объектов класса в список в словарей
+    recipes_to_json = []
+    for r in recipes:
+        recipes_to_json.append(r.__dict__)
     with open('recipes.json', 'w', encoding='utf-8') as f:
-        json.dump(recipes, f, ensure_ascii=False, indent=4)
+        json.dump(recipes_to_json, f, ensure_ascii=False, indent=4)
+    
+    print(f'Рецепт "{recipe.title}" успешно сохранен!')
 
-    print(f'Рецепт успешно создан. ID: {new_id}')
-    return new_id
 
 def get_recipe_by_id(target_id: int):
     for recipe in recipes:
-        if recipe['id'] == target_id:
+        if recipe.id == target_id:
             print('Рецепт найден')
             return recipe
-    raise RecipeNotFoundError(f'ID {target_id} не существует(')
+    raise RecipeNotFoundError(f'ID {target_id} не существует')
+
 
 def filter_by_time(max_time: int):
     approve_recipes = []
     for recipe in recipes:
-        if recipe['time'] <= max_time:
+        if recipe.time <= max_time:
             approve_recipes.append(recipe)
-
-    if approve_recipes == []:
-        return 'Нет подходящих рецептов'
-    
     return approve_recipes
-
-recipe1 = {
-    'title': 'Ночная овсянка',
-    'ingredients': {'Овсянка': 70, 'Творожок': 100, 'Молоко': 100},
-    'instructions': 'Тут инструкция',
-    'time': 5
-}
-
-try:
-    create_recipe(recipe1)
-except InvalidRecipeError:
-    print('Некорректный рецепт')
-except InvalidCookingTimeError:
-    print('Время приготовления должно быть > 0 минут')
+    
